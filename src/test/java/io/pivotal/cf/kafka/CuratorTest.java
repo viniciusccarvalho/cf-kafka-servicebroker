6466a8b955b9157707a89c59cce823627387dc76
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2015-01-02 19:19:32 -0500

    Fixing external zkServer dependency

--- src/test/java/io/pivotal/cf/kafka/CuratorTest.java
+++ src/test/java/io/pivotal/cf/kafka/CuratorTest.java
@@ -2,13 +2,12 @@ package io.pivotal.cf.kafka;
 
 import io.pivotal.cf.broker.model.ServiceInstance;
 
-import java.util.concurrent.CountDownLatch;
-
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.framework.api.CuratorEvent;
-import org.apache.curator.framework.api.CuratorListener;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.retry.RetryOneTime;
+import org.apache.curator.test.TestingServer;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,15 +15,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class CuratorTest {
 
 	private ObjectMapper mapper = new ObjectMapper();
+	private TestingServer zkServer;
+	private CuratorFramework cli;
+	
+	@Before
+	public void startZookeeper() throws Exception {
+		zkServer = new TestingServer(2181);
+	    cli = CuratorFrameworkFactory.newClient(zkServer.getConnectString(), new RetryOneTime(2000));
+	}
 	
 	@Test
 	public void writeServiceDefinition() throws Exception {
-		ExponentialBackoffRetry retryPolicy = new ExponentialBackoffRetry(1000, 3);
-		CuratorFramework client = CuratorFrameworkFactory.newClient("localhost:2181", retryPolicy);
-		client.start();
+		cli.start();
 		
 		ServiceInstance instance = new ServiceInstance("1", "1", "1", "uuid-1", "uuid-1", "http://google.com");
-		client.create().creatingParentsIfNeeded().forPath("/sb/instances/"+instance.getId(), mapper.writeValueAsBytes(instance));
+		cli.create().creatingParentsIfNeeded().forPath("/sb/instances/"+instance.getId(), mapper.writeValueAsBytes(instance));
 	}
 	
 }
