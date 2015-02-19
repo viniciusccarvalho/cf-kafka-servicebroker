5703641438903a8a260268170b32f89f7568ee62
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2015-02-19 17:00:30 -0500

    Fixing service description typo

---
+++ src/test/java/io/pivotal/cf/kafka/services/impl/ServiceManagementImplTest.java
@@ -0,0 +1,12 @@
+package io.pivotal.cf.kafka.services.impl;
+
+import io.pivotal.cf.broker.Application;
+
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+
+@IntegrationTest
+@SpringApplicationConfiguration(classes=Application.class)
+public class ServiceManagementImplTest {
+
+}
